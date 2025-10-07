d04f7c2c0498387e7dffcfe323e146bcc20dc031
Luiz Eduardo de Menescal <luiz.menescal@impactcommerce.com>
2025-10-07 10:39:19 +0100

    Fix TS error in SpeedInsights: Remove invalid 'route' prop (auto-tracked by package)

--- src/components/insights/speed-insights.tsx
+++ src/components/insights/speed-insights.tsx
@@ -1,9 +1,7 @@
 'use client';
 
 import { SpeedInsights } from '@vercel/speed-insights/next';
-import { usePathname } from 'next/navigation';
 
 export function SpeedInsightsComponent() {
-  const pathname = usePathname();
-  return <SpeedInsights route={pathname} />;
+    return <SpeedInsights />;
 }
